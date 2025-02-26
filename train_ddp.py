7a9b3339ba0c04ec2487bbe57f5c297e53bbb022
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-02-26 06:40:43 +0000

    add muon to ddp
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- train_ddp.py
+++ train_ddp.py
@@ -28,17 +28,20 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 from zeroband.world_info import get_world_info
 
+from torch import Tensor
 
-class AdamConfig(BaseConfig):
-    type: Literal["adam"] = "adam"
-    lr: float = 4e-4
-    weight_decay: float = 0.1
-    betas1: float = 0.9
-    betas2: float = 0.95
+
+class MuonConfig(BaseConfig):
+    type: Literal["muon"] = "muon"
+    lr: float = 2e-2
+    weight_decay: float = 0.01
+    momentum: float = 0.95
+    nesterov: bool = True
+    ns_steps: int = 5
 
 
 class OptimConfig(BaseConfig):
-    optim: AdamConfig = AdamConfig()
+    optim: MuonConfig = MuonConfig()
     sched_type: Literal["cosine", "linear", "wsd-sqrt"] = "cosine"
     warmup_steps: int = 1000
     stable_steps: int = 80_000
@@ -70,6 +73,122 @@ class TrainingProgress:
     total_tokens: int
     outer_step: int
     step: int
+
+
+def zeropower_via_newtonschulz5(G: Tensor, steps: int) -> Tensor:
+    """
+    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
+    performance at all relative to UV^T, where USV^T = G is the SVD.
+    """
+    assert (
+        G.ndim >= 2
+    )  # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
+    a, b, c = (3.4445, -4.7750, 2.0315)
+    X = G.bfloat16()
+    if G.size(-2) > G.size(-1):
+        X = X.mT
+
+    # Ensure spectral norm is at most 1
+    X = X / (X.norm(dim=(-2, -1), keepdim=True) + 1e-7)
+    # Perform the NS iterations
+    for _ in range(steps):
+        A = X @ X.mT
+        B = (
+            b * A + c * A @ A
+        )  # quintic computation strategy adapted from suggestion by @jxbz, @leloykun, and @YouJiacheng
+        X = a * X + B @ X
+
+    if G.size(-2) > G.size(-1):
+        X = X.mT
+    return X
+
+
+class Muon(torch.optim.Optimizer):
+    """
+    Muon - MomentUm Orthogonalized by Newton-schulz
+
+    https://kellerjordan.github.io/posts/muon/
+
+    Muon internally runs standard SGD-momentum, and then performs an orthogonalization post-
+    processing step, in which each 2D parameter's update is replaced with the nearest orthogonal
+    matrix. To efficiently orthogonalize each update, we use a Newton-Schulz iteration, which has
+    the advantage that it can be stably run in bfloat16 on the GPU.
+
+    Some warnings:
+    - This optimizer should not be used for the embedding layer, the final fully connected layer,
+    or any {0,1}-D parameters; those should all be optimized by a standard method (e.g., AdamW).
+    - To use it with 4D convolutional filters, it works well to just flatten their last 3 dimensions.
+
+    Arguments:
+        lr: The learning rate used by the internal SGD.
+        momentum: The momentum used by the internal SGD.
+        nesterov: Whether to use Nesterov-style momentum in the internal SGD. (recommended)
+        ns_steps: The number of Newton-Schulz iteration steps to use.
+    """
+
+    def __init__(
+        self, params, lr=0.02, weight_decay=0.01, momentum=0.95, nesterov=True, ns_steps=5, rank=0, world_size=1
+    ):
+        self.rank = rank
+        self.world_size = world_size
+        defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov, ns_steps=ns_steps)
+        params: list[Tensor] = [*params]
+        param_groups = []
+        for size in {p.numel() for p in params}:
+            b = torch.empty(world_size, size, dtype=torch.bfloat16, device="cuda")
+            group = dict(
+                params=[p for p in params if p.numel() == size],
+                update_buffer=b,
+                update_buffer_views=[b[i] for i in range(world_size)],
+            )
+            param_groups.append(group)
+        super().__init__(param_groups, defaults)
+
+    @torch.no_grad()
+    def step(self):
+        for group in self.param_groups:
+            update_buffer: Tensor = group["update_buffer"]
+            update_buffer_views: list[Tensor] = group["update_buffer_views"]
+            # generate weight updates in distributed fashion
+            params: list[Tensor] = group["params"]
+            handle = None
+            params_world = None
+
+            def update_prev():  # optimized Muon implementation contributed by @YouJiacheng
+                handle.wait()
+                for p_world, g_world in zip(params_world, update_buffer_views):
+                    p_world.mul_(1 - group["lr"] * group["weight_decay"])
+                    p_world.add_(
+                        g_world.view_as(p_world),
+                        alpha=-group["lr"] * max(1, p_world.size(-2) / p_world.size(-1)) ** 0.5,
+                    )
+
+            for base_i in range(len(params))[:: self.world_size]:
+                if base_i + self.rank < len(params):
+                    p = params[base_i + self.rank]
+                    g = p.grad
+                    assert g is not None
+                    state = self.state[p]
+                    if "momentum_buffer" not in state:
+                        state["momentum_buffer"] = torch.zeros_like(g)
+                    buf: Tensor = state["momentum_buffer"]
+                    buf.lerp_(g, 1 - group["momentum"])
+                    g = g.lerp_(buf, group["momentum"]) if group["nesterov"] else buf
+                    if g.ndim == 4:  # for the case of conv filters
+                        g = g.view(len(g), -1)
+                    g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten()
+                else:
+                    g = update_buffer_views[self.rank]
+                if base_i > 0:
+                    update_prev()  # async all_gather instead of sync all_reduce by @YouJiacheng
+                handle = dist.all_gather_into_tensor(update_buffer, g, async_op=True)
+                params_world = params[base_i : base_i + self.world_size]
+            update_prev()
 
 
 def train(config: Config):
@@ -127,20 +246,40 @@ def train(config: Config):
     logger.info(f"Initializing DDP model on device {world_info.local_rank}")
     model = DDP(model, device_ids=[world_info.local_rank], broadcast_buffers=False, gradient_as_bucket_view=True)
 
-    optimizer = torch.optim.AdamW(
-        params=model.parameters(),
+    hidden_matrix_params = [p for n, p in model.module.layers.named_parameters() if p.ndim >= 2 and "embed" not in n]
+    embed_params = [p for n, p in model.module.named_parameters() if "embed" in n]
+    scalar_params = [p for p in model.module.parameters() if p.ndim < 2]
+    head_params = [model.module.output.weight]
+
+    # init the optimizer(s)
+    adam_params = [
+        dict(params=head_params, lr=0.008),
+        dict(params=embed_params, lr=0.6),
+        dict(params=scalar_params, lr=0.04),
+    ]
+    optimizer1 = torch.optim.Adam(adam_params, betas=(0.8, 0.95), eps=1e-10, fused=True)
+    optimizer2 = Muon(
+        hidden_matrix_params,
         lr=config.optim.optim.lr,
-        weight_decay=config.optim.optim.weight_decay,
-        betas=(config.optim.optim.betas1, config.optim.optim.betas2),
+        momentum=config.optim.optim.momentum,
+        nesterov=config.optim.optim.nesterov,
+        ns_steps=config.optim.optim.ns_steps,
+        rank=world_info.rank,
+        world_size=world_info.world_size,
     )
 
-    scheduler = get_scheduler(
-        sched_type=config.optim.sched_type,
-        optimizer=optimizer,
-        num_warmup_steps=config.optim.warmup_steps,
-        num_stable_steps=config.optim.stable_steps,
-        num_training_steps=config.optim.total_steps,
-    )
+    optimizers = [optimizer2, optimizer1]
+
+    schedulers = [
+        get_scheduler(
+            sched_type=config.optim.sched_type,
+            optimizer=optimizer,
+            num_warmup_steps=config.optim.warmup_steps,
+            num_stable_steps=config.optim.stable_steps,
+            num_training_steps=config.optim.total_steps,
+        )
+        for optimizer in optimizers
+    ]
 
     training_progress = TrainingProgress(total_tokens=0, outer_step=0, step=0)
 
@@ -185,10 +324,11 @@ def train(config: Config):
 
         grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)  # type: ignore (is a dtensor)
 
-        optimizer.step()
-        scheduler.step()
+        for optimizer, scheduler in zip(optimizers, schedulers):
+            optimizer.step()
+            scheduler.step()
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
         # logging
         training_progress.step += 1
